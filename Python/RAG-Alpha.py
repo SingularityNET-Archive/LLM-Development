915ce22d0cb6339868a6ff4669bc892903d47d44
swhitenstall <swhitenstall@ekphrasis.co.uk>
2024-04-24 16:50:58 +0100

    Initial code commit - to problem output in VS Code

--- Python/RAG-Alpha.py
+++ Python/RAG-Alpha.py
@@ -1,1 +1,8 @@
 # This is a proof of concept RAG (retrieval-augmented generation) file
+# See https://python.langchain.com/docs/use_cases/question_answering/quickstart/
+
+# Install dependencies
+
+pip install -qU langchain-openai;
+
+# Set environment variables
